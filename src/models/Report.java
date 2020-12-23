6cf58b450dc10c64db5147ef6c52671555c09ccc
kim.ryosu <r.kimura0524@gmail.com>
2020-12-23 18:56:54 +0900

    Add Report index

--- src/models/Report.java
+++ src/models/Report.java
@@ -8,9 +8,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-@Table(name = "employees")
+@Table(name = "reports")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllReports",
+            query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
+            ),
+    @NamedQuery(
+            name = "getReportsCount",
+            query = "SELECT COUNT(r) FROM Report AS r"
+            )
+})
 @Entity
 public class Report {
     @Id
